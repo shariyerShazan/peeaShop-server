7a8e09ce106bcdd16ab7c8ccd0a7afc11e6601e1
shariyerShazan <shariyershazan1@gmail.com>
2025-08-31 19:23:57 +0600

    server make by nodejs-express and run it

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ import dotenv from "dotenv"
 dotenv.config()
 import express from "express"
 import cors from "cors"
-
+import cookieParser from "cookie-parser"
 
 // middlewares
 const app = express()
 app.use(express.json())
+app.use(cookieParser())
 app.use(express.urlencoded({extended: true}))
 app.use(cors({
     origin: "http://localhost:5173" ,
