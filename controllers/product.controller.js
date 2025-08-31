b7664805a812eb69dc52454740171f6923889911
shariyerShazan <shariyershazan1@gmail.com>
2025-09-01 02:11:24 +0600

    product controller setup

---
+++ controllers/product.controller.js
@@ -0,0 +1,15 @@
+export const createProduct = async (req , res)=>{
+
+}
+
+export const getAllProducts = async (req , res)=>{
+
+}
+
+export const updateProduct = async (req , res)=>{
+
+}
+
+export const deleteProduct = async (req , res)=>{
+    
+}
--- models/product.model.js
+++ models/product.model.js
@@ -43,6 +43,11 @@ const productSchema = new mongoose.Schema({
                 type: String ,
                 required: true
             },
+            createdBy : {
+                type: mongoose.Schema.Types.ObjectId ,
+                ref: "User" ,
+                required : true
+            }
             
 }, {timestamps: true})
 
